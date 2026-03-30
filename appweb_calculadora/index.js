af7844a909aca5da1f186461ec0074352389595e
CARLOS MIGUEL FELISBINO SOARES <carlos.felisbino@a.ucb.br>
2026-03-30 20:39:53 -0300

    projeto calculadora dia 30-03

--- appweb_calculadora/index.js
+++ appweb_calculadora/index.js
@@ -1,17 +1,48 @@
+// Carrega express framework da internet
 const express = require('express');
 
+// 
+const calculadora = require('./calculadora')
+
+// Cria um app do tipo express
 const app = express()
 
-const PORT = 8080;
+// Porta de execução
+const PORT = 3000;
 
+//Crie um array para armazenar resposta ao usuário
+const LOG = [];
+
+// '/' define rota raiz
 app.get('/', (req, res) => {
-    res.send("<h1>Hello, World!</h1> <p>Estou na teia... VAI TOMANDO AÍ DENTRO!!!</p>");
+    res.send("<h1>Hello, World!</h1> <p>Estou na teia(web)... VAI TOMANDO AÍ DENTRO!!!</p>");
 });
 
-app.get('/bem-vindo', (req, res) => {
-    res.send("<h1>BEM VINDO, CACHORRÃO HAHAHAHAHA</h1>");
+// URL de soma
+app.get('/calculadora/somar/:a/:b', (req, res) => {
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    let resultado = calculadora.somar(a, b);
+    let string_resultado = `<h1>${a} + ${b} = ${resultado}</h1>`
+    LOG.push(string_resultado)
+    res.send(string_resultado);
 })
 
+// URL de LOG
+app.get('/calculadora/log', (req, res) => {
+    resultado = "";
+    LOG.forEach(log => {
+        resultado += log;
+    });
+    res.send(resultado);
+})
+
+// URL com paramentro
+app.get('/ola/:nome', (req, res) => {
+    res.send(`<h1>BEM VINDO, ${req.params.nome} HAHAHAHAHA</h1>`);
+})
+
+// Inicializar a aplicação na porta definida
 app.listen(PORT, () => {
     console.log('app rodando na porta ' + PORT);
 });
