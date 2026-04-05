ddf0d472b5eb97abff32b0d6f1277bed4e562686
carlosmiguelfelisbino <carlosmiguelfelisbino@gmail.com>
2026-04-05 01:54:34 -0300

    atividade estoque incompleta

--- appweb_calculadora/index.js
+++ appweb_calculadora/index.js
@@ -1,17 +1,19 @@
 // Carrega express framework da internet
 const express = require('express');
 
-// 
-const calculadora = require('./calculadora')
+// Carrega o modulo "calculadora"
+const calculadora = require('./calculadora');
 
 // Cria um app do tipo express
-const app = express()
+const app = express();
 
 // Porta de execução
 const PORT = 3000;
 
-//Crie um array para armazenar resposta ao usuário
-const LOG = [];
+// Inicializar a aplicação na porta definida
+app.listen(PORT, () => {
+    console.log('app rodando na porta ' + PORT);
+});
 
 // '/' define rota raiz
 app.get('/', (req, res) => {
@@ -24,9 +26,12 @@ app.get('/calculadora/somar/:a/:b', (req, res) => {
     let b = Number(req.params.b);
     let resultado = calculadora.somar(a, b);
     let string_resultado = `<h1>${a} + ${b} = ${resultado}</h1>`
-    LOG.push(string_resultado)
+    LOG.push(string_resultado);
     res.send(string_resultado);
-})
+});
+
+//Crie um array para armazenar resposta ao usuário
+const LOG = [];
 
 // URL de LOG
 app.get('/calculadora/log', (req, res) => {
@@ -35,15 +40,32 @@ app.get('/calculadora/log', (req, res) => {
         resultado += log;
     });
     res.send(resultado);
-})
+});
 
 // URL com paramentro
 app.get('/ola/:nome', (req, res) => {
     res.send(`<h1>BEM VINDO, ${req.params.nome} HAHAHAHAHA</h1>`);
-})
-
-// Inicializar a aplicação na porta definida
-app.listen(PORT, () => {
-    console.log('app rodando na porta ' + PORT);
 });
 
+const estoque = [];
+
+app.get('/adicionar/:id/:nome/:qtd', (req, res) => {
+    let id = req.params.id;
+    let nome = req.params.nome;
+    let qtd = req.params.qtd;
+    let resultado_estoque = `<h2>Id: ${id}</h2>\n<h2>Nome: ${nome}</h2>\n<h2>Quantidade: ${qtd}</h2>`;
+    estoque.push(resultado_estoque);
+    res.send("Cadastrado com sucesso!");
+});
+
+app.get('/listar', (req, res) => {
+    resultado = ""
+    estoque.forEach(log => {
+        resultado += log;
+    });
+    res.send(resultado);
+});
+
+app.get('/remover/:id', (req, res) => {
+    
+});
